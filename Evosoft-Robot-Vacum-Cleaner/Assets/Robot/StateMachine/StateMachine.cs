6981d31567dfcd43269a150f76811996734329e6
Milán Kormány <kormany.1993@gmail.com>
2022-05-05 23:11:55 +0200

    Added AddTransition Method for Transition change in State Machine

--- Evosoft-Robot-Vacum-Cleaner/Assets/Robot/StateMachine/StateMachine.cs
+++ Evosoft-Robot-Vacum-Cleaner/Assets/Robot/StateMachine/StateMachine.cs
@@ -12,9 +12,11 @@ public class StateMachine
 
     public void AddTransition(IState fromState, IState toState, Func<bool> condition)
     {
-        if (_transitions.TryGetValue(from.GetType(), out var transitions) == false)
+        if (_transitions.TryGetValue(fromState.GetType(), out var transitions) == false)
         {
-
+            transitions = new List<Transition>();
+            _transitions[fromState.GetType()] = transitions;
         }
+        transitions.Add(new Transition(toState, condition));
     }
 }
